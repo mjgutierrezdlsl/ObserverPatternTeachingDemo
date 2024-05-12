b789e3ab9917156d25fb91c11bb5e40567e9cb8c
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2024-05-12 16:45:26 +0800

    adjusted visibility of increment score

--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -6,7 +6,7 @@ public class ScoreHandler : MonoBehaviour
     public int Score { get; private set; }
     public TextMeshProUGUI _scoreDisplay;
 
-    private void IncrementScore(float _)
+    public void IncrementScore()
     {
         Score++;
         _scoreDisplay.text = $"{Score:000}";
